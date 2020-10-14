2fa9e5622ca23d3679057471be1109e9379021ba
Matthew Chau <26354088+TOLOTOLO999@users.noreply.github.com>
2020-10-15 04:07:01 +0800

    debug chromedriver

--- utils/get_cookies.py
+++ utils/get_cookies.py
@@ -5,7 +5,10 @@ import pprint as pp
 
 
 def get_cookies(url):
-    driver = webdriver.Chrome(CHROME_DRIVER_PATH)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=chrome_options)
     driver.get(url)
     cookies = driver.get_cookies()
     driver.close()
